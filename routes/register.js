9c5136c9425fc98acb43e415a15584bb54fa3587
Riasat Ullah <riasat.ullah@gmail.com>
2014-04-29 02:24:09 -0400

    I have completed the RegisterForm. Once the user submits his information, register.js file checks to see if the entered username already exists or not. If it does not then the registration is complete and the user is directed back to the login page to login. However, if the entered username already exists then the user is directed to RegisterFormError page where he is asked to change his username.
    Besides, I have added the separate fields for birthdate on the EditProfile page. For the same reason, I have also had to change the users.js page a little.
    I have simply added in the columns that the PersonalInformation table in the database was missing.

--- routes/register.js
+++ routes/register.js
@@ -12,43 +12,81 @@ module.exports = function(request,response) {
     //User Information
     var firstName = request.body.firstName;
     var lastName= request.body.lastName;
-    var birthday = request.body.birthday;
+    var birthday_day = request.body.birthday_day;
+    var birthday_month = request.body.birthday_month;
+    var birthday_year = request.body.birthday_year;
     var gender = request.body.gender;
     var phoneNumber = request.body.phoneNumber;
     var address = request.body.address;
-    var driverExperiance = request.body.driverExperiance;
+    var driverExperience = request.body.driverExperience;
     var email = request.body.email;
-    var aboutMe = request.body.aboutMe;
+    var aboutMe = request.body.AboutMe;
     
-    var SessionUsername = request.session.username;
     
-    if (SessionUsername) {
-        console.log("person is logged in");
-        users.update(SessionUsername, firstName, lastName, birthday,
-                 gender, phoneNumber, address,
-                 driverExperiance, email, aboutMe,
-                 function(success) {
-                    
-        if (success) {
-            response.redirect('/');
-        }
-        //If error during profile information change. Print Error to screen.
-        });
-    }else{
-        console.log("person is not logged in");
-        users.create(username, password, confirmPassword,
-                 firstName, lastName, birthday,
-                 gender, phoneNumber, address,
-                 driverExperiance, email, aboutMe,
-                 function(success) {
+    //if the username already exists, then redirect the user back to the register form
+    //otherwise, register his information and redirect him to the login page to login
+    users.getUserInformation(username, function(user){
         
-        if (success) {
-            request.session.username = username;
-        } else {
-            //This Error always gets set. WHY??????
-            request.session.error = 'Username '+username+' is not available. Or password doesnt match';
+        if (user) {
+	    
+	    var datalist = {};
+	    datalist.firstName = firstName;
+	    datalist.lastName = lastName;
+	    datalist.birthday_day = birthday_day;
+	    datalist.birthday_month = birthday_month;
+	    datalist.birthday_year = birthday_year;
+	    datalist.gender = gender;
+	    datalist.phoneNumber = phoneNumber;
+	    datalist.address = address;
+	    datalist.driverExperience = driverExperience;
+	    datalist.email = email;
+	    datalist.aboutMe = aboutMe;
+	    
+            response.render('RegisterFormError',{datalist:datalist});
+        }else{
+	    users.create(username, password, confirmPassword,
+                 firstName, lastName, birthday_day, birthday_month, birthday_year,
+                 gender, phoneNumber, address, driverExperience, email, aboutMe,
+                 function(success) {
+	            if (success) {
+		        request.session.username = username;
+		    }
+		}
+	    );
+	    response.redirect('/');
         }
-            response.redirect('/');
-        });
-    }
+    });
+
 };
+
+
+    
+//    var SessionUsername = request.session.username;
+//    
+//    
+//    if (SessionUsername) {
+//        console.log("person is logged in");
+//        users.update(SessionUsername, firstName, lastName, birthday_day, birthday_month, birthday_year,
+//                 gender, phoneNumber, address, driverExperiance, email, aboutMe,
+//                 function(success) {
+//                    
+//        if (success) {
+//            response.redirect('/');
+//        }
+//        //If error during profile information change. Print Error to screen.
+//        });
+//    }else{
+//        console.log("person is not logged in");
+//        users.create(username, password, confirmPassword,
+//                 firstName, lastName, birthday_day, birthday_month, birthday_year,
+//                 gender, phoneNumber, address, driverExperiance, email, aboutMe,
+//                 function(success) {
+//	            if (success) {
+//		        request.session.username = username;
+//		    } else {
+//		        //This Error always gets set. WHY??????
+//		        request.session.error = 'Username '+username+' is not available. Or password doesnt match';
+//		    }
+//	        response.redirect('/');
+//        });
+//    };
