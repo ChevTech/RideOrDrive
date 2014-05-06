941db8d5226bcd647caac791abf2ce8f32c37e32
Anton Stoytchev <Anton.Stoytchev@gmail.com>
2014-05-06 17:36:39 -0400

    Fixed current posts page

--- routes/getCurrentPosts.js
+++ routes/getCurrentPosts.js
@@ -13,25 +13,35 @@ module.exports = function(request,response) {
     
             //Compute the current date
             var getDate = new Date();
-            var year  = getDate.getFullYear();
-            var month = getDate.getMonth() + 1;
-            var day   = getDate.getDate();
-            var currentDate = year + "-" + month + "-" + day;
+            var currentYear  = getDate.getFullYear();
+            var currentMonth = getDate.getMonth() + 1;
+            var currentDay   = getDate.getDate();
+   
    
             //Get the current rider posts
             RiderPosts.forEach(function(post) {
-                if (currentDate <= post.DepartureDate){
-                    currentRiderPosts.push(post);   		
-                   }
+                var postDate  = post.DepartureDate.split("-");
+                var postYear  = postDate[0];
+                var postMonth = postDate[1];
+                var postDay   = postDate[2];
+                
+                if (currentYear <= postYear && currentMonth <= postMonth && currentDay <= postDay){
+                        currentRiderPosts.push(post);   		
+                    }
             });
             
             //Get the current driver posts
             DriverPosts.forEach(function(post) {
-                if (currentDate <= post.DepartureDate){
-                    currentDriverPosts.push(post);   		
-                   }
+                var postDate  = post.DepartureDate.split("-");
+                var postYear  = postDate[0];
+                var postMonth = postDate[1];
+                var postDay   = postDate[2];
+                
+                if (currentYear <= postYear && currentMonth <= postMonth && currentDay <= postDay){
+                        currentDriverPosts.push(post);   		
+                    }
             });
-   
+        
             response.render('ViewCurrentPosts', {currentRiderPosts:currentRiderPosts, currentDriverPosts:currentDriverPosts});
         });
     }); 
