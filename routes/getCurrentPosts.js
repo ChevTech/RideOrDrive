aba3ce92994d1eae0c97d812618b4ae611bbf986
Anton Stoytchev <Anton.Stoytchev@gmail.com>
2014-05-06 16:48:20 -0400

    Fixed changes to Create a post

--- routes/getCurrentPosts.js
+++ routes/getCurrentPosts.js
@@ -5,23 +5,34 @@ module.exports = function(request,response) {
     
     var username = request.session.username;
     
-    posts.retrieveUserPosts(username, function(posts){
-        
-         var currentPosts = [];
+    posts.retrieveRiderPosts(username, function(RiderPosts){
+        posts.retrieveDriverPosts(username, function(DriverPosts){
+            
+            var currentRiderPosts  = [];
+            var currentDriverPosts = [];
     
-   	 //Compute the current date
-   	 var getDate = new Date();
-	 var year  = getDate.getFullYear();
-   	 var month = getDate.getMonth() + 1;
-   	 var day   = getDate.getDate();
-   	 var currentDate = year + "-" + month + "-" + day;
-
-         posts.forEach(function(post) {
-             if (currentDate <= post.DepartureDate){
-                 currentPosts.push(post);   		
-		}
-       	});
-
-	 response.render('ViewCurrentPosts', {currentPosts:currentPosts});
-	});
+            //Compute the current date
+            var getDate = new Date();
+            var year  = getDate.getFullYear();
+            var month = getDate.getMonth() + 1;
+            var day   = getDate.getDate();
+            var currentDate = year + "-" + month + "-" + day;
+   
+            //Get the current rider posts
+            RiderPosts.forEach(function(post) {
+                if (currentDate <= post.DepartureDate){
+                    currentRiderPosts.push(post);   		
+                   }
+            });
+            
+            //Get the current driver posts
+            DriverPosts.forEach(function(post) {
+                if (currentDate <= post.DepartureDate){
+                    currentDriverPosts.push(post);   		
+                   }
+            });
+   
+            response.render('ViewCurrentPosts', {currentRiderPosts:currentRiderPosts, currentDriverPosts:currentDriverPosts});
+        });
+    }); 
 };
